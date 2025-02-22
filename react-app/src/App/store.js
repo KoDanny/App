2c87974c3cb7daf1bed448b2699d18e03e2fe6e0
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-02-22 18:11:15 +0300

    Connection DEV TOOLS

--- react-app/src/App/store.js
+++ react-app/src/App/store.js
@@ -1,5 +1,7 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './Reducers';
 import { thunk } from 'redux-thunk';
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
